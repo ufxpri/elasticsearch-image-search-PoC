334c7b854b7aa8bc0919b0979e6ada7435e86e2d
ufxpri <cfi2000@naver.com>
2023-02-09 12:44:09 +0000

    add kibana

--- actions/start_elasticsearch.sh
+++ actions/start_elasticsearch.sh
@@ -1,13 +1,5 @@
-# sudo docker run \
-#     -d \
-#     --rm \
-#     -p 9200:9200 \
-#     -p 9300:9300 \
-#     -e "discovery.type=single-node" \
-#     docker.elastic.co/elasticsearch/elasticsearch:7.17.9
+sudo docker network create elasticsearch
 
-sudo docker network create elastic
-sudo docker pull docker.elastic.co/elasticsearch/elasticsearch:7.17.9
 sudo docker run -d --rm \
     --name es01-test \
     --net elastic \
@@ -16,10 +8,9 @@ sudo docker run -d --rm \
     -e "discovery.type=single-node" \
     docker.elastic.co/elasticsearch/elasticsearch:7.17.9
 
-sudo docker pull docker.elastic.co/kibana/kibana:7.17.9
 sudo docker run -d --rm \
     --name kib01-test \
     --net elastic \
-    -p 127.0.0.1:5601:5601 \
+    -p 0.0.0.0:5601:5601 \
     -e "ELASTICSEARCH_HOSTS=http://es01-test:9200" \
     docker.elastic.co/kibana/kibana:7.17.9
